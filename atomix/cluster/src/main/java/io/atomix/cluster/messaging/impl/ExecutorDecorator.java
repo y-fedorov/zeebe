d0c4393399ee114ecc6633350fb095c5c52c5777
Christopher Zell <zelldon91@googlemail.com>
2022-12-20 11:04:37 +0100

    decorate executor

---
+++ atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/ExecutorDecorator.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.atomix.cluster.messaging.impl;
+
+import java.util.concurrent.Executor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class ExecutorDecorator implements Executor {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+  private final NettyMessagingService nettyMessagingService;
+  private final Executor delegate;
+
+  public ExecutorDecorator(
+      final NettyMessagingService nettyMessagingService, final Executor delegate) {
+    this.nettyMessagingService = nettyMessagingService;
+    this.delegate = delegate;
+  }
+
+  @Override
+  public void execute(final Runnable runnable) {
+    try {
+      delegate.execute(runnable);
+    } catch (Exception ex) {
+      log.error("Error on executing runnable {}", runnable, ex);
+    }
+  }
+}
