e293771e6a5f3727122634dec5589217a59d81b3
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-12-20 19:24:55 +0100

    improve streamer performance

--- clients/java/src/main/java/io/camunda/zeebe/client/impl/ZeebeClientImpl.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/impl/ZeebeClientImpl.java
@@ -356,7 +356,7 @@ public final class ZeebeClientImpl implements ZeebeClient {
   @Override
   public StreamJobsCommandStep1 newStreamJobsCommand() {
     return new StreamJobsCommandImpl(
-        asyncStub, config, jsonMapper, credentialsProvider::shouldRetryRequest);
+        asyncStub, config, jsonMapper, executorService, credentialsProvider::shouldRetryRequest);
   }
 
   private JobClient newJobClient() {
